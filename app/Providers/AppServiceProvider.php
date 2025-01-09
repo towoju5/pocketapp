36ea1e7876e0348c47a3a236de743305b1e2e7e0
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-01-09 23:47:53 +0100

    App service provider adjuster

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,27 +23,29 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (tableExists('options')) {
+            $u_option = [];
             $options = Option::where("autoload", "yes")->get();
             foreach ($options as $option) {
-                config(["option" => [$option->option_name => $option->option_value]]);
+                $u_option[] = [$option->option_name => $option->option_value];
             }
 
             // Share user wallets with all views
-            View::composer('*', function ($view) {
+            View::composer('*', function ($view) use(&$u_option) {
                 if (Auth::check()) {
                     $user = Auth::user();
-                    $wallets = [
+                    $uwallets = [
                         'all_wallets' => $user->wallets,
-                        'main_wallets' => $user->wallets()->where('type', 'main')->get(),
-                        'forex_mt4_wallets' => $user->wallets()->where('type', 'forex_mt4')->get(),
-                        'forex_mt5_wallets' => $user->wallets()->where('type', 'forex_mt5')->get(),
-                        'shares_trading_wallets' => $user->wallets()->where('type', 'shares_trading')->get(),
+                        'main_wallets' => $user->wallets()->where('description', 'main')->get(),
+                        'forex_mt4_wallets' => $user->wallets()->where('description', 'forex_mt4')->get(),
+                        'forex_mt5_wallets' => $user->wallets()->where('description', 'forex_mt5')->get(),
+                        'shares_trading_wallets' => $user->wallets()->where('description', 'shares_trading')->get(),
                         'active_wallet' => $user->wallets()->where('currently_active', true)->first(),
                         'live_wallets' => $user->wallets()->where('mode', 'live')->get(),
                         'test_wallets' => $user->wallets()->where('mode', 'test')->get()
                     ];
 
-                    $view->with('wallets', $wallets);
+                    $view->with('uwallets', $uwallets);
+                    $view->with('u_option', $u_option);
                 }
             });
         }
