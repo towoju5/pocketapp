a941862bd01740f5953b08b275885d4b6f96a4e4
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-01-09 23:42:19 +0100

    DB Seeder enabled

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             BitgoSeeder::class,
-            // AssetSeeder::class,
-            // CountryStateCityTableSeeder::class,
+            AssetSeeder::class,
+            CountryStateCityTableSeeder::class,
         ]);
     }
 }
