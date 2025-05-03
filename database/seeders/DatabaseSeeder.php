98d446d968981c0fe7d6f6b2bca007adaadb4865
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-05-03 09:20:53 +0100

    user address field casted as array

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
-            TradeSeeder::class,
+            // UserSeeder::class,
+            // TradeSeeder::class,
             BitgoSeeder::class,
             AssetSeeder::class,
             // CountryStateCityTableSeeder::class,
