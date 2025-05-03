e811824ace86e5c44c1dee5ea6ce8fd63332733f
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-05-03 09:10:02 +0100

    user seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            UserSeeder::class,
             TradeSeeder::class,
             BitgoSeeder::class,
             AssetSeeder::class,
