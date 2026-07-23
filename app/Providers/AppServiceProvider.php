09d1f966490f2d38892c8c5a97c85f4888072639
Emmanuel A Towoju <towojuads@gmail.com>
2026-07-23 01:34:54 +0100

    major updates to design and functionality

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,35 +25,35 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Check if the options table exists before querying
-        // if (Schema::hasTable('options')) {
-        //     $social_trades = social_trades();
-        //     View::composer('*', compact('social_trades'));
-        //     $u_option = [];
-        //     $options = Option::where("autoload", "yes")->get();
-        //     foreach ($options as $option) {
-        //         $u_option[$option->option_name] = $option->option_value;
-        //     }
+        if (Schema::hasTable('options')) {
+            $social_trades = social_trades();
+            View::composer('*', compact('social_trades'));
+            $u_option = [];
+            $options = Option::where("autoload", "yes")->get();
+            foreach ($options as $option) {
+                $u_option[$option->option_name] = $option->option_value;
+            }
 
-        //     // Share data with all views
-        //     View::composer('*', function ($view) use ($u_option) {
-        //         $wallet_balance = ["balance" => 0]; // Default wallet balance
+            // Share data with all views
+            View::composer('*', function ($view) use ($u_option) {
+                $wallet_balance = ["balance" => 0]; // Default wallet balance
 
-        //         if (Auth::check()) {
-        //             $user = Auth::user();
+                if (Auth::check()) {
+                    $user = Auth::user();
 
-        //             $uwallets = $user->wallets();
+                    $uwallets = $user->wallets();
 
-        //             // Fetch user's wallet balance
-        //             $wallet_balance = $user->getWallet($user->active_wallet_slug ?? 'qt_demo_usd') ?? ["balance" => 0];
+                    // Fetch user's wallet balance
+                    $wallet_balance = $user->getWallet($user->active_wallet_slug ?? 'qt_demo_usd') ?? ["balance" => 0];
 
-        //             $view->with('_user', $user);
-        //             $view->with('uwallets', $uwallets);
-        //             $view->with('wallet_balance', $wallet_balance);
-        //         }
+                    $view->with('_user', $user);
+                    $view->with('uwallets', $uwallets);
+                    $view->with('wallet_balance', $wallet_balance);
+                }
 
-        //         // Share options regardless of authentication
-        //         $view->with('u_option', $u_option);
-        //     });
-        // }
+                // Share options regardless of authentication
+                $view->with('u_option', $u_option);
+            });
+        }
     }
 }
