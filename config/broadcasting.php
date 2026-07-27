dbd96e26077230990b4d2da1f7919afd7f9a583c
Emmanuel A Towoju <towojuads@gmail.com>
2026-07-27 03:57:58 +0100

    debugging reverb connection

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,9 +37,9 @@ return [
             'app_id' => env('REVERB_APP_ID'),
             'options' => [
                 'host' => env('REVERB_HOST'),
-                'port' => env('REVERB_PORT', 443),
-                'scheme' => env('REVERB_SCHEME', 'https'),
-                'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+                'port' => env('REVERB_PORT', 8080),
+                'scheme' => env('REVERB_SCHEME', 'http'),
+                'useTLS' => false, //env('REVERB_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
